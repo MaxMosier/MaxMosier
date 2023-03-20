853b4e8de42749f0c2c43296bf7a92897c57eeb8
MaxMosier <112131580+MaxMosier@users.noreply.github.com>
2023-03-19 22:38:15 -0600

    Removed "degree"
    Removed the word "degree" from my template text after rereading everything and realizing how redundant it sounded with the temperature units already present. It's the wrong order, anyways.

--- js/wa9.js
+++ js/wa9.js
@@ -23,7 +23,7 @@ const possibleObjects = [
 
 // Default story template:
 const storyLayout =
-  "The fog was thick in the :insertSize: pine forest, making it hard to see beyond a few feet. :insertName: shivered in the :insertTemp: degree weather, regretting not bringing a warmer coat. :userName: had insisted on coming here, to the eerie :insertPlace: where they had heard rumors of a :insertObject:. As they stumbled through the mist, they heard a rustling in the bushes. :insertName: tried to call out, but their voice was stuck in their throat. Suddenly, :userName: grabbed their arm and whispered urgently, \"We need to :insertVerb: now!\"";
+  "The fog was thick in the :insertSize: pine forest, making it hard to see beyond a few feet. :insertName: shivered in the :insertTemp: weather, regretting not bringing a warmer coat. :userName: had insisted on coming here, to the eerie :insertPlace: where they had heard rumors of a :insertObject:. As they stumbled through the mist, they heard a rustling in the bushes. :insertName: tried to call out, but their voice was stuck in their throat. Suddenly, :userName: grabbed their arm and whispered urgently, \"We need to :insertVerb: now!\"";
 // ^^ The name ":userName:" is what I've used in place of "Bob" left intact as default value
 
 const customName = document.getElementById("customname");
