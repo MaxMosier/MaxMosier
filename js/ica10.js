ec6f39b3c8e95d85d935485033324c99cc822627
MaxMosier <112131580+MaxMosier@users.noreply.github.com>
2023-03-22 20:15:11 -0600

    Not Sure...
    I've mainly just reformatted this file with ALT+SHIFT+F, and re-pasted some of the content from the given template on the MDN page (https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Build_your_own_function). No idea why it's saying pretty much everything was changed, though. Perhaps it has something to do with the formatter's having replaced every "'" with """? Not sure yet, but don't feel the need to diagnose it atm...

--- js/ica10.js
+++ js/ica10.js
@@ -1,30 +1,34 @@
-function displayMessage(inpuText, messageType){
-    const body = document.body;
+function displayMessage(inpuText, messageType) {
+  const body = document.body;
 
-    const panel = document.createElement('div');
-    panel.setAttribute('class','msgBox');
-    body.appendChild(panel);
-    
-    const msg = document.createElement('p');
-    msg.textContent = inpuText;
-    panel.appendChild(msg);
-    
-    const closeBtn = document.createElement('button');
-    closeBtn.textContent = 'x';
-    panel.appendChild(closeBtn);
-    
-    closeBtn.addEventListener('click', () => panel.parentNode.removeChild(panel));
+  const panel = document.createElement("div");
+  panel.setAttribute("class", "msgBox");
+  body.appendChild(panel);
 
-    if (msgType === 'warning') {
-        msg.style.backgroundImage = 'url(../img/warning.png)';
-        panel.style.backgroundColor = 'red';
-      } else if (msgType === 'chat') {
-        msg.style.backgroundImage = 'url(icons/chat.png)';
-        panel.style.backgroundColor = 'aqua';
-      } else {
-        msg.style.paddingLeft = '20px';
-      }
+  const msg = document.createElement("p");
+  msg.textContent = "This is a message box";
+  panel.appendChild(msg);
+
+  const closeBtn = document.createElement("button");
+  closeBtn.textContent = "x";
+  panel.appendChild(closeBtn);
+
+  closeBtn.addEventListener("click", () => panel.parentNode.removeChild(panel));
+
+  if (msgType === "warning") {
+    msg.style.backgroundImage = "url(../img/warning.png)";
+    panel.style.backgroundColor = "red";
+  } else if (msgType === "chat") {
+    msg.style.backgroundImage = "url(icons/chat.png)";
+    panel.style.backgroundColor = "aqua";
+  } else {
+    msg.style.paddingLeft = "20px";
+  }
 }
 
-const button = document.querySelector('button');
-btn.addEventListener('click', () => displayMessage('Woo this is a different message!'))
+// displayMessage();
+
+const button = document.querySelector("button");
+btn.addEventListener("click", () =>
+  displayMessage("Woo this is a different message!")
+);
