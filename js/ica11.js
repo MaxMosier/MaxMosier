f9f2ee4875c233b07c4af0e666a556c8bf83e15b
Maximillian Mosier <mamo4416@colorado.edu>
2023-04-04 15:58:26 +0000

    Changed some of the text on the ica11 html, and started writing the js!

---
+++ js/ica11.js
@@ -0,0 +1,8 @@
+const button = document.querySelector("#js-quote-text");
+
+button.addEventListener('click', fetchQuestion);
+
+function fetchQuestion(){
+    console.log("The button and event call are working as intended so far!");
+}
+
