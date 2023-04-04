0b8136db5cb24e3693b819e138382b8af0a76fc3
Maximillian Mosier <mamo4416@colorado.edu>
2023-04-04 16:04:31 +0000

    Just added the API endpoint declared as a string variable

--- js/ica11.js
+++ js/ica11.js
@@ -1,6 +1,7 @@
 const button = document.querySelector("#js-new-quote");
-
 button.addEventListener('click', fetchQuestion);
+
+const apiEndpoint= "https://trivia.cyberwisp.com/getrandomchristmasquestion";
 
 function fetchQuestion(){
     console.log("The button and event call are working as intended so far!");
