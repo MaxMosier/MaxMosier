e7c82d9bbbd73ee0fae22f775e89e2a14e3db7a9
MaxMosier <112131580+MaxMosier@users.noreply.github.com>
2023-04-05 22:36:23 -0600

    And a Simple Fix
    First off, all of the change it says was made is mainly because I hit ALT + SHIFT + F to reformat my code.
    
    Anyways, The changes I've made are as follows:
    
    - In the HTML, I just added a new div within the quote box to contain the answer (As opposed to my previous solution, which was to append the answer to the content of the existing div, preceding that with a newline character.
    
    - The 'json' variable is no longer local to the fetchContent function, for the sake of preserving the answer after the function returns, for future use in revealing said answer.
    
    - New json is only fetched if the fetchContent function is set to "question" mode (Meaning it's been passed a number greater than 0)
    
    - Used the above changes to take the answer from the existing json and use it as the content for the #js-answer-text div

--- js/ica11.js
+++ js/ica11.js
@@ -1,39 +1,43 @@
 const qButton = document.querySelector("#js-new-quote");
-qButton.addEventListener('click', () => fetchContent(1));
+qButton.addEventListener("click", () => fetchContent(1));
 const aButton = document.querySelector("#js-tweet"); //I'll change the name later if I've the time to.
-aButton.addEventListener('click', () => fetchContent(0));
+aButton.addEventListener("click", () => fetchContent(0));
 
-const apiEndpoint= "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+const apiEndpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
 
-async function fetchContent(questionOrAnswer){
-    try {
-        const obtainedQuestion = await fetch(apiEndpoint);
-        if(!obtainedQuestion.ok){
-            throw Error(obtainedQuestion.statusText);
-        }
-        const json = await obtainedQuestion.json();
-        if(questionOrAnswer > 0){ //If input is greater than 0, gives question
-            displayQuestion(json.question);
-            console.log("New question given");
-        }
-        else{ //If equal to or less than 0, gives answer
-            displayAnswer(json.answer);
-            console.log("New answer given");
-        }
-    } catch(err){
-        console.log(err);
-        alert("Failed to fetch the new question!");
+let json = "";
+
+async function fetchContent(questionOrAnswer) {
+  try {
+    if (questionOrAnswer > 0) {
+      //If input is greater than 0, gives question
+      const obtainedQuestion = await fetch(apiEndpoint);
+      if (!obtainedQuestion.ok) {
+        throw Error(obtainedQuestion.statusText);
+      }
+      json = await obtainedQuestion.json();
+
+      displayQuestion(json.question);
+      displayAnswer("");
+      console.log("New question given");
+    } else {
+      //If equal to or less than 0, gives answer
+      displayAnswer(json.answer);
+      console.log("New answer given");
     }
+  } catch (err) {
+    console.log(err);
+    alert("Failed to fetch the new question!");
+  }
 }
 
-function displayQuestion(inQuestion){
-    const questionText = document.querySelector("#js-quote-text");
-    questionText.textContent = inQuestion;
+function displayQuestion(inQuestion) {
+  const questionText = document.querySelector("#js-quote-text");
+  questionText.textContent = inQuestion;
 }
 
-function displayAnswer(inAnswer){
-    let questionText = document.querySelector('#js-quote-text');
-    inAnswer = '\n' + inAnswer; // Add newline
-    questionText.textContent += inAnswer;
+function displayAnswer(inAnswer) {
+  let questionText = document.querySelector("#js-answer-text");
+  questionText.textContent = inAnswer;
 }
 fetchContent(1);
