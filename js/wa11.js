65b1134c592e654c46462c0316857f615e05cd54
MaxMosier <112131580+MaxMosier@users.noreply.github.com>
2023-04-09 22:51:17 -0600

    Reformatted
    Just reformatted the page. None of the content itself has been changed.

--- js/wa11.js
+++ js/wa11.js
@@ -6,41 +6,44 @@ const long = 92;
 const url = `https://api.weather.gov/gridpoints/GJT/${lat},${long}/forecast`;
 
 // Get the forecast data from the API
-function refreshWeatherInfo(){
-fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-        // console.log(data); // Was using this for debugging. Don't need it right now, but am keeping it!
-        console.log("Weather data refreshing...");
+function refreshWeatherInfo() {
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+			// console.log(data); // Was using this for debugging. Don't need it right now, but am keeping it!
+			console.log("Weather data refreshing...");
 
-        // Extract the data! It's stored as "periods"
-        const forecast = data.properties.periods;
+			// Extract the data! It's stored as "periods"
+			const forecast = data.properties.periods;
 
-        // Get the div elements to update
-        const daysAbove = Array.from(document.querySelectorAll(".upper-day-zone"));
-        const daysBelow = Array.from(document.querySelectorAll(".lower-day-zone"));
-        const dayNames = Array.from(document.querySelectorAll(".day-name"));
+			// Get the div elements to update
+			const daysAbove = Array.from(
+				document.querySelectorAll(".upper-day-zone")
+			);
+			const daysBelow = Array.from(
+				document.querySelectorAll(".lower-day-zone")
+			);
+			const dayNames = Array.from(document.querySelectorAll(".day-name"));
 
-        // Update each day element with the forecast data
-        for (let i = 1; i <= daysAbove.length*2; i += 2) {
+			// Update each day element with the forecast data
+			for (let i = 1; i <= daysAbove.length * 2; i += 2) {
+				const dayIndex = Math.ceil(i / 2) - 1;
 
-            const dayIndex = Math.ceil(i/2)-1;
+				const upperData = forecast[i];
+				const highTemp = upperData.temperature;
+				daysAbove[dayIndex].textContent = `High: ${highTemp}°F`;
 
-            const upperData = forecast[i];
-            const highTemp = upperData.temperature;
-            daysAbove[dayIndex].textContent = `High: ${highTemp}°F`;
+				const lowerData = forecast[i + 1];
+				const lowTemp = lowerData.temperature;
+				daysBelow[dayIndex].textContent = `Low: ${lowTemp}°F`;
 
-            const lowerData = forecast[i+1];
-            const lowTemp = lowerData.temperature;
-            daysBelow[dayIndex].textContent = `Low: ${lowTemp}°F`;
+				const dayOfWeek = upperData.name;
+				dayNames[dayIndex].textContent = dayOfWeek;
+			}
 
-            const dayOfWeek = upperData.name;
-            dayNames[dayIndex].textContent = dayOfWeek;
-        }
-
-        console.log("Weather data refreshed!");
-    })
-    .catch((error) => console.error(error));
+			console.log("Weather data refreshed!");
+		})
+		.catch((error) => console.error(error));
 }
 
 refreshWeatherInfo();
