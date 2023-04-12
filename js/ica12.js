44a354318b76887777f8b9b2f71a4f1a125c9d0f
Maximillian Mosier <mamo4416@colorado.edu>
2023-04-12 22:29:01 +0000

    As of the last commit, this still isn't working. However, I noticed that I for some reason put parenthesis around the 'canvas.width/height = window.innerWidth/Height' part, so maybe that was the problem? I'll know after this push

--- js/ica12.js
+++ js/ica12.js
@@ -1,8 +1,8 @@
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
-const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
 
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
