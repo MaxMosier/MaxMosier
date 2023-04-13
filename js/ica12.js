81eb8886c101c5baa6d63b6451fdd4900b5248b8
MaxMosier <112131580+MaxMosier@users.noreply.github.com>
2023-04-12 23:21:06 -0600

    Position Update Change and Collision Beginning
    Updated the updatePosition() function to now make the bouncing of the balls slightly more realistic.
    
    Also added the code given for the collision handling. Haven't done anything with it yet, but will!

--- js/ica12.js
+++ js/ica12.js
@@ -45,44 +45,64 @@ class Ball {
     updatePosition() {
         if (this.x + this.size >= width) {
             this.vX = -this.vX;
+            this.x = width-this.size;
         }
 
         if (this.x - this.size <= 0) {
             this.vX = -this.vX;
+            this.x = this.size;
         }
 
         if (this.y + this.size >= height) {
             this.vY = -this.vY;
+            this.y = height-this.size;
         }
 
         if (this.y - this.size <= 0) {
             this.vY = -this.vY;
+            this.y = this.size;
         }
 
         this.x += this.vX;
         this.y += this.vY;
     }
+
+    collisionDetect() {
+        for (const ball of ballSet) {
+           if (!(this === ball)) {
+              const dx = this.x - ball.x;
+              const dy = this.y - ball.y;
+              const distance = Math.sqrt(dx * dx + dy * dy);
+  
+              if (distance < this.size + ball.size) {
+                ball.h += 10;
+                this.h += 10;
+              }
+           }
+        }
+     }
+
 }
 
 let ballSet = [];
 
-for (let i = 0; i < 100; i++) {
-    let ballSize = random(8, 28);
+for (let i = 0; i < 500; i++) {
+    let ballSize = random(8, 14);
     let newBall = new Ball(
         random(0, width),
         random(0, height),
         randomSign()*random(1, 5),
         randomSign()*random(1, 5),
         ballSize,
-        random(240, 360),
-        random(20,40),
-        random(20, 80)
+        random(60, 300),
+        random(40,60),
+        random(30, 80)
     )
     ballSet.push(newBall);
 }
 
 function drawloop(){
-    ctx.fillStyle = "#0004";
+    ctx.fillStyle = "#0001";
     ctx.fillRect(0,0,width,height);
 
     for(const ball of ballSet){
