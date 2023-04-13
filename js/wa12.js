ee33b1025d576ad9df394c71aeda445ddb1bd016
Maximillian Mosier <mamo4416@colorado.edu>
2023-04-13 19:44:51 +0000

    Done wit hstep four, so I've just got five and six to go! Accidentally combined steps 1 and 2, though, so I'll be separating those later.

--- js/wa12.js
+++ js/wa12.js
@@ -1,4 +1,6 @@
-let jsonString = `{
+// Step 1: Make the JSON
+// (I wrote it as a string to start, for the sake of practicing doing so)
+const jsonString = `{
     "companyName": "Tech Stars",
     "website": "www.techstars.site",
     "employees": [
@@ -42,3 +44,39 @@ let newEmployee = {
     "raiseElligible" : false
 };
 console.log(newEmployee);
+companyObject.employees.push(newEmployee);
+
+console.log("So the company employee list is now:");
+console.log(companyObject.employees);
+
+console.log("Calculating sum of employee salaries...");
+
+let salarySum = 0;
+
+for(const individual of companyObject.employees){
+    salarySum += individual.salary;
+}
+
+console.log("Sum of employee salaries is " + addCommas(salarySum));
+
+
+function addCommas(num) {
+
+    const str = num.toString();
+    let output = "";
+    let pos = 0; // (Out current position in the string)
+    
+    // Loop through the string representation of the integer from !!RIGHT TO LEFT!!
+    for (let i = str.length - 1; i >= 0; i--) {
+      pos++;
+      // I'll continuously build to the new output
+      output = str[i] + output;
+      // Couldn't use += because I need to add to the left side
+      // If we've seen three digits add a comma
+      if (pos % 3 === 0 && i !== 0) //Unless we're at the leftmost digit, 
+      {
+        output = ',' + output;
+      }
+    }
+    return output;
+}
