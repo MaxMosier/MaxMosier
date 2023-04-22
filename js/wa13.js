0170d8ccc1d04b6762b7f4f75df67f3012383056
MaxMosier <112131580+MaxMosier@users.noreply.github.com>
2023-04-22 12:35:23 -0600

    Prototype Finished
    I didn't really make that many pushes, but have so far completed a basic, functional outline of what I want to do for this assignment. I'm gonna' change a few things up, but it works decently at the moment.
    
    One change that is to come will include a complete refactoring of the JS. Instead of doing what it does now to determine the volume, which is to average the interval between the past X clicks, I need it to count the number of clicks over the past X (milli)seconds. Completely different concept!

--- js/wa13.js
+++ js/wa13.js
@@ -1,72 +1,80 @@
-function check() {
-    console.log('test');
-}
+// Global variables
+let lastClickTimestamp = 0;
+let started = false;
+let timeTable = Array(3).fill(-1); // -1 will be the default value to indicate sections of the array not yet given values
+let currentIndex = 0; //Adds the 
+const tapButton = document.getElementById('tapButton');
+const volumeDisplay = document.getElementById('volumeDisplay');
+const audioPlayer = document.getElementById('audioPlayer');
 
-function submit() {
-    alert("Volume set to: " + output.textContent);
-}
 
-function reset() {
-    outputInt = 0;
-    output.textContent = outputInt;
-}
-
-function minus() {
-    if (outputInt > 0) {
-    outputInt -=1;
-    output.textContent = outputInt; }
-    
-}
-
-function plus() {
-    if (outputInt < 100) {
-    outputInt +=1;
-    output.textContent = outputInt;
+// A simple mapping function:
+function remap(originVal, inputMin, inputMax, outputMin, outputMax, clamp = true){
+    let outputVal = originVal;
+    if(clamp){
+        if(originVal > inputMax){
+            outputVal = inputMax;
+        }
+        if(originVal < inputMin){
+            outputVal = inputMin; 
+        }
     }
+    return ( ( (outputVal - inputMin) * (outputMax - outputMin) ) / (inputMax - inputMin) ) + outputMin;
 }
 
-function random() {
-    outputInt = randomNumber(0, 100);
-    output.textContent = outputInt;
+// Click event listener
+tapButton.addEventListener('click', () => {
+    const currentTimestamp = performance.now(); // Timestamp in milliseconds
+
+    if (!started) { // If we haven't yet received the first click
+        lastClickTimestamp = currentTimestamp; // Set the timestamp
+        started = true; // And change 'started' to true to indicate the first click has been received and "start" the timers
+        audioPlayer.play();
+        return;
+    }
+
+    const newestInterval = currentTimestamp - lastClickTimestamp;
+    console.log(`Newest interval: ${newestInterval}`);
+    enqueueTimeDifference(currentTimestamp - lastClickTimestamp);
+    lastClickTimestamp = currentTimestamp;
+    updateVolume();
+});
+
+// Adds the newest time to the list of times. Order doesn't matter since we just want the average
+function enqueueTimeDifference(timeDifference) {
+    timeTable[currentIndex] = timeDifference;
+    currentIndex++; // Move up one in the index
+    currentIndex %= timeTable.length; // Restrict to the bounds of the array
 }
 
-function randomNumber(min, max) {
-    const num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return num;
-  }
+// Volume functions
+function updateVolume() {
+    let sum = 0;
+    let observedElements = 0;
 
+    for (let i = 0; i < timeTable.length; i++) {
+        if (timeTable[i] == -1) {
+            break;
+        }
+        sum += timeTable[i];
+        observedElements++;
+    }
 
+    const average = sum / observedElements;
+    console.log(`\t Current average: ${average}`);
+    let normalizedVolume = remap(average, 20, 1000, 100, 0);
 
-const output = document.querySelector('.output');
-let outputInt = parseInt(output.textContent);
-console.log(outputInt);
-
-const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
-const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
-const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
-const randomButton = document.querySelector('.random-button').addEventListener('click', random);
-const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
-
-
-/* const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
-
-button.addEventListener('click', updateOutput);
-
-function updateOutput() {
-    output.textContent = phone_content.value;
-    alert(phone_content.value);
+    setVolumeTo(normalizedVolume);
 }
-*/
 
-
-var slider = document.getElementById("myRange");
-var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
-var sliderOutput = document.querySelector(".slider-output");
-
-
-// Update the current slider value (each time you drag the slider handle)
-function update() {
-  sliderOutput.textContent = slider.value;
+function setVolumeTo(volume) {
+    audioPlayer.volume = volume / 100.0;
+    volumeDisplay.textContent = volume;
+    updateVolumeBar(volume);
 }
+
+function updateVolumeBar(volume) {
+    const volumeBar = document.getElementById('volumeBar');
+    volumeBar.style.height = volume + '%';
+}
+
