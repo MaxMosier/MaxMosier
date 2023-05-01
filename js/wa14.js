e4c78645f122208f64501b50065d382ce8831ff7
MaxMosier <112131580+MaxMosier@users.noreply.github.com>
2023-04-30 23:24:27 -0600

    Progress
    I've got the basic page set up. I'm just gonna' do a bit more styling, but wanted to get it pushed while I have it working.

--- js/wa14.js
+++ js/wa14.js
@@ -1,11 +1,7 @@
-mapboxgl.accessToken =
-    "pk.eyJ1IjoibWF4d3giLCJhIjoiY2xneWxreWlhMDd2MTNmbm1yanc4a3JqZyJ9.brbAhB8h2g970I8JZI-7FA";
-
+mapboxgl.accessToken = "pk.eyJ1IjoibWF4d3giLCJhIjoiY2xneWxreWlhMDd2MTNmbm1yanc4a3JqZyJ9.brbAhB8h2g970I8JZI-7FA";
 const map = new mapboxgl.Map({
-    container: "map", // container ID
-    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: "mapbox://styles/maxwx/clgyov7tx004z01rh8fv15kl3", // style URL
-    center: [11.94,53.70238], // starting position [lng, lat]
-    zoom: 9, // starting zoom
+	container: "map",
+	style: "mapbox://styles/maxwx/clh4e30lu009s01r89lc888ii",
+	center: [-1.935, 53.71],
+	zoom: 9.8,
 });
-//
